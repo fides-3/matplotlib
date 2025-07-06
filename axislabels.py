278f372661b7087266a3bc7317b79f3dada3dfb1
ntoiti <fidesluckia36@gmail.com>
2025-07-06 14:13:30 +0300

    added axis labels

---
+++ axislabels.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+year=[1950,1951,1952,2100]
+pop=[2.53,2.57,2.62,10.85]
+
+plt.plot(year,pop)
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.title("World Population Projection")
+
+plt.show()
